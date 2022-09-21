a96a699b59c210376bbea24845bd00bea9b352b0
tenymakuach <tenymakuach@gmail.com>
2022-09-21 01:39:10 -0700

    4-puts.c modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Print message
- * Return: 0
- */
+ *
+ * * main - Print message
+ *
+ * * Return: 0
+ *
+ * */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual language");
-Return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
